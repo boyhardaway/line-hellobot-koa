eb7653acbad3b566da1bf6908bb46277090f23d5
boyhardaway <boy.hardaway@gmail.com>
2018-06-28 10:05:01 +0700

    aa

--- src/route/chatbot/message.js
+++ src/route/chatbot/message.js
@@ -4,7 +4,7 @@ const client = new line.Client({
   channelAccessToken: '{M9MipC2I8aSvbYB65LR56z5IScjq4U+ZU4uigiwhEx7D9zImwJw7doAlRmcMj6F7jFITY6cZdrHru1NKGkbaFEMfDQRwzwbw9/YFyO+JW1BuZ4hDoAbL12H3cdv5Y3/BmnClCZrzYKsEXgv3dcLifQdB04t89/1O/w1cDnyilFU=}'
 })
 
-// const replyMessage = (replyToken) => { 
+const replyMessage = (replyToken) => { 
 //   const message = {
 //     type: 'uri',
 //     label:"View details",
@@ -33,13 +33,13 @@ const client = new line.Client({
     ],
   })
   //https://line.me/R/app/1586994800-WboBOnRK
-  client.replyMessage(replyToken, message)
-    .then(() => {
-      console.log('.then') 
-    })
-    .catch((err) => {
-      console.log(err) 
-    })  
+  // client.replyMessage(replyToken, message)
+  //   .then(() => {
+  //     console.log('.then') 
+  //   })
+  //   .catch((err) => {
+  //     console.log(err) 
+  //   })  
 }
 
 

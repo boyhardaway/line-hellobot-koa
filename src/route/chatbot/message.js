a5624cff28130460f90da52b0ed8fa81842f9212
boyhardaway <boy.hardaway@gmail.com>
2018-06-28 10:12:12 +0700

    aa

--- src/route/chatbot/message.js
+++ src/route/chatbot/message.js
@@ -4,10 +4,10 @@ const client = new line.Client({
   channelAccessToken: '{M9MipC2I8aSvbYB65LR56z5IScjq4U+ZU4uigiwhEx7D9zImwJw7doAlRmcMj6F7jFITY6cZdrHru1NKGkbaFEMfDQRwzwbw9/YFyO+JW1BuZ4hDoAbL12H3cdv5Y3/BmnClCZrzYKsEXgv3dcLifQdB04t89/1O/w1cDnyilFU=}'
 })
 
-const client = LineClient.connect({
+const clientv2 = LineClient.connect({
   accessToken: '{M9MipC2I8aSvbYB65LR56z5IScjq4U+ZU4uigiwhEx7D9zImwJw7doAlRmcMj6F7jFITY6cZdrHru1NKGkbaFEMfDQRwzwbw9/YFyO+JW1BuZ4hDoAbL12H3cdv5Y3/BmnClCZrzYKsEXgv3dcLifQdB04t89/1O/w1cDnyilFU=}',
   channelSecret: '6a38ce8a1610100c07534812b9e042b9',
-});
+})
 
 const replyMessage = (replyToken) => { 
 //   const message = {
@@ -16,7 +16,7 @@ const replyMessage = (replyToken) => {
 //     uri: 'https://www.google.co.th'
 //   }
 
-  client.replyButtonTemplate(replyToken, 'this is a template', { 
+  clientv2.replyButtonTemplate(replyToken, 'this is a template', { 
     title: 'Menu',
     text: 'Please select',
     actions: [

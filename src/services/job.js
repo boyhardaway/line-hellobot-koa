45182d7ae8fc33af6448a8092c498c9139859ca4
boyhardaway <boy.hardaway@gmail.com>
2018-07-10 10:16:30 +0700

    A

--- src/services/job.js
+++ src/services/job.js
@@ -3,9 +3,10 @@ const message = require('../route/chatbot/message')
 
 // var rule = new schedule.RecurrenceRule();
 // rule.second = 20
-const runJob = schedule.scheduleJob('* 15 * * * *', function(){
+const runJob = schedule.scheduleJob('*/5 * * * * *', function(){
   console.log(`The answer to life, the universe, and everything!`) 
-  message.replyFlexMessage('U82d78fbd0a435be08b2b5b22f122da30')
+  message.replyMessage('U82d78fbd0a435be08b2b5b22f122da30')
+  runJob.cancel()
 })
 
 module.exports = runJob

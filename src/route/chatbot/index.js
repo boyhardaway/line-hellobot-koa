ae6e4c8a25b031cde07dae66de58652ec2294d56
boyhardaway <boy.hardaway@gmail.com>
2018-07-18 10:34:36 +0700

    a

--- src/route/chatbot/index.js
+++ src/route/chatbot/index.js
@@ -1,11 +1,19 @@
 const Router = require('koa-router')
+const middlewareLine = require('@line/bot-sdk').middleware
+const config = require('../../config/config')
 const webhooks = require('./webhooks') 
 const SendMessageAuction = require('./SendMessageAuction')
 const router = new Router()
  
 console.log("==============Index router===============")
+
+const configAccess = {
+    channelAccessToken: config.channelAccessToken,
+    channelSecret: config.channelSecret
+  }
+
 //Post 
-router.post('/webhooks', webhooks.postHandler)  
+router.post('/webhooks', middlewareLine(configAccess), webhooks.postHandler)  
 
 //Get
 // router.get('/webhooks', webhooks.getHandler)  
--- src/route/chatbot/messagingApi.js
+++ src/route/chatbot/messagingApi.js
@@ -1,4 +1,4 @@
-const line = require('@line/bot-sdk'); 
+const line = require('@line/bot-sdk');
 const config = require('../../config/config')
 const client = new line.Client({
   channelAccessToken: '{' + config.channelAccessToken + '}'

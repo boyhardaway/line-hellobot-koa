2b94ef7e59538a4787386cf92acec49e2c4700f4
boyhardaway <boy.hardaway@gmail.com>
2018-07-18 14:42:54 +0700

    aa

--- src/services/scheduleJob.js
+++ src/services/scheduleJob.js
@@ -2,7 +2,7 @@ const schedule = require('node-schedule')
 const message = require('../route/chatbot/messagingApi')
 const flexMessage = require('../route/chatbot/flexMessage') 
  
-const scheduleJobSendMessage = schedule.scheduleJob('* 10 * * * *', function(){
+const scheduleJobSendMessage = schedule.scheduleJob('* * * * * *', function(){
  
   message.pushMessage('U82d78fbd0a435be08b2b5b22f122da30', flexMessage.messageAuction)
   // scheduleJobSendMessage.cancel() 

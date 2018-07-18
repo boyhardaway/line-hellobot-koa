d2072dbb0bee5197e360b993cb5cfbce4cfd1998
boyhardaway <boy.hardaway@gmail.com>
2018-07-18 09:07:27 +0700

    a

--- src/services/scheduleJob.js
+++ src/services/scheduleJob.js
@@ -9,7 +9,7 @@ const scheduleJobSendMessage = schedule.scheduleJob('*/5 * * * * *', function(){
   
   message.pushMessage('U82d78fbd0a435be08b2b5b22f122da30', flexMessage.messageAuction)
   // message.pushMessage('Ub2ced7a7b935f98c5fa5b8a97dc56af4', flexMessage.messageAuction)
-  // scheduleJobSendMessage.cancel()
+  scheduleJobSendMessage.cancel()
 
 
   //dome Ub2ced7a7b935f98c5fa5b8a97dc56af4

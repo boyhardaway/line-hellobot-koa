6849fe766076a27871f99da06f2da57e2ebabab9
boyhardaway <boy.hardaway@gmail.com>
2018-07-18 09:14:11 +0700

    a

--- src/route/chatbot/SendMessageAuction.js
+++ src/route/chatbot/SendMessageAuction.js
@@ -4,7 +4,7 @@ const flexMessage = require('./flexMessage')
 const AutoSendMsg = () => {
      
   message.pushMessage('U82d78fbd0a435be08b2b5b22f122da30', flexMessage.messageAuction)
-  // message.pushMessage('Ub2ced7a7b935f98c5fa5b8a97dc56af4', flexMessage.messageAuction)
+  message.pushMessage('U60db7ce1175f33efdcd717037724715c', flexMessage.messageAuction)
   
 }
 
